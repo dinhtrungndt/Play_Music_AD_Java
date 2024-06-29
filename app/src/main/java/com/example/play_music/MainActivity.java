d5b27c23e94e8316ea281584df84ae119b6b227d
kho quản lý <nguyendinhtrung.it@gmail.com>
2024-06-29 16:56:37 +0700

    up

---
+++ app/src/main/java/com/example/play_music/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.play_music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView play, prev, next, imageView;
+    TextView songTitle;
+    SeekBar mSeekBarTime, mSeekBarVol;
+    static MediaPlayer mMediaPlayer;
+    private Runnable runnable;
+    private AudioManager mAudioManager;
+    int currentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        // initializing views
+
+        play = findViewById(R.id.play);
+        prev = findViewById(R.id.prev);
+        next = findViewById(R.id.next);
+        songTitle = findViewById(R.id.songTitle);
+        imageView = findViewById(R.id.imageView);
+        mSeekBarTime = findViewById(R.id.seekBarTime);
+        mSeekBarVol = findViewById(R.id.seekBarVol);
+
+        // creating an ArrayList to store our songs
+
+        final ArrayList<Integer> songs = new ArrayList<>();
+
+        songs.add(0, R.raw.rememberme);
+        songs.add(1, R.raw.amthambenem);
+        songs.add(2, R.raw.thereare);
+
+        // intializing mediaplayer
+
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+
+
+
+        // seekbar volume
+
+        int maxV = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curV = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        mSeekBarVol.setMax(maxV);
+        mSeekBarVol.setProgress(curV);
+
+        mSeekBarVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        //above seekbar volume
+        //
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSeekBarTime.setMax(mMediaPlayer.getDuration());
+                if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.pause();
+                    play.setImageResource(R.drawable.play_btn);
+                } else {
+                    mMediaPlayer.start();
+                    play.setImageResource(R.drawable.ic_baseline_stop_24);
+                }
+
+                songNames();
+
+            }
+        });
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.ic_baseline_stop_24);
+                }
+
+                if (currentIndex < songs.size() - 1) {
+                    currentIndex++;
+                } else {
+                    currentIndex = 0;
+                }
+
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+
+                mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+
+                mMediaPlayer.start();
+                songNames();
+
+            }
+        });
+
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.ic_baseline_stop_24);
+                }
+
+                if (currentIndex > 0) {
+                    currentIndex--;
+                } else {
+                    currentIndex = songs.size() - 1;
+                }
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+
+                mMediaPlayer = MediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+                mMediaPlayer.start();
+                songNames();
+
+            }
+        });
+
+    }
+
+    private void songNames() {
+        if (currentIndex == 0) {
+            songTitle.setText("Remember Me - Sơn tùng MTP");
+            imageView.setImageResource(R.drawable.remember);
+        }
+        if (currentIndex == 1) {
+            songTitle.setText("Âm thầm bên em - Sơn tùng MTP");
+            imageView.setImageResource(R.drawable.amtham);
+        }
+        if (currentIndex == 2) {
+            songTitle.setText("There's No One At All - Sơn tùng MTP");
+            imageView.setImageResource(R.drawable.thereare);
+        }
+
+        // seekbar duration
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mSeekBarTime.setMax(mMediaPlayer.getDuration());
+                mMediaPlayer.start();
+            }
+        });
+
+        mSeekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mMediaPlayer.seekTo(progress);
+                    mSeekBarTime.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mMediaPlayer != null) {
+                    try {
+                        if (mMediaPlayer.isPlaying()) {
+                            Message message = new Message();
+                            message.what = mMediaPlayer.getCurrentPosition();
+                            handler.sendMessage(message);
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @SuppressLint("Handler Leak")
+    Handler handler = new Handler () {
+        @Override
+        public void handleMessage  (Message msg) {
+            mSeekBarTime.setProgress(msg.what);
+        }
+    };
+}
